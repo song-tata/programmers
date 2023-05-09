c228ea87ecaf0e8d880b8f0f5173dc85343b06da
song_tata <kgsong8020@naver.com>
2023-05-10 08:52:37 +0900

    init : level 1
    프로그래머스 레벨 1 추억 점수문제

---
+++ src/programers/level1/missyou.java
@@ -0,0 +1,14 @@
+package programers.level1;
+
+public class missyou {
+
+	public int[] solution(String[] name, int[] yearning, String[][] photo) {
+		int[] answer = {};
+		return answer;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
