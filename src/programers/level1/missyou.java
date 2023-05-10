384c96b4a39bae1765232e1ce90ebc5e978177c3
song_tata <kgsong8020@naver.com>
2023-05-10 09:02:48 +0900

    feat : photo length 알아보기
    수업 시작이라 중간저장

--- src/programers/level1/missyou.java
+++ src/programers/level1/missyou.java
@@ -4,11 +4,18 @@ public class missyou {
 
 	public int[] solution(String[] name, int[] yearning, String[][] photo) {
 		int[] answer = {};
+		for (int i = 0; i < photo.length; i++) {
+
+		}
+
 		return answer;
 	}
 
 	public static void main(String[] args) {
-
+		String[] name = {};
+		int[] yearning = {};
+		String[][] photo = {{"may", "kein", "kain", "radi"},{"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
+		
+		System.out.println(photo.length);
 	}
-
 }
